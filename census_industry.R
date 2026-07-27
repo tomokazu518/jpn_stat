f5c489e3999d5b59963b15cc200846d7e6182702
Tomokazu Nomura <tomokazu.nomura@gmail.com>
2026-07-27 21:21:23 +0900

    Adjust linewidth in census_industry.R for bar plots to enhance visual clarity and aesthetics.

--- census_industry.R
+++ census_industry.R
@@ -132,7 +132,11 @@ clr <- c(brewer.pal(11, "Paired"), brewer.pal(8, "YlOrBr"), "#FFFFFF")
 graph_industry <- industry |>
   mutate(year = as.factor(year)) |>
   ggplot(aes(x = year, y = value, fill = industry)) +
-  geom_bar(stat = "identity", color = "black") +
+  geom_bar(
+    stat = "identity",
+    color = "black",
+    linewidth = 0.25
+  ) +
   scale_fill_manual(name = "産業", values = clr) +
   scale_x_discrete(guide = guide_axis(n.dodge = 2)) +
   labs(x = "年", y = "") +
